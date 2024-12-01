95d5c03ad8c5d2b2fdf41a60eb99db2d1861ae76
Ankit <ankit.singh@leverageedu.com>
2024-12-01 14:47:03 +0530

    Db migration added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
+const db = require('./models/db');
 dotenv.config();
 
 const app = express();
@@ -15,5 +16,6 @@ app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+  db.migrate.latest();
   console.log(`Server is running on port ${PORT}`);
 });
