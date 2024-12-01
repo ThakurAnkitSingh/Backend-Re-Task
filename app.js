496209f2ab9568ea3a964195f3656e57cde14d8a
Ankit <ankit.singh@leverageedu.com>
2024-12-01 20:14:23 +0530

    added cors

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://frontend-reunion-task.onrender.com',
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
